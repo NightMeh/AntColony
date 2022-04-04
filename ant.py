410e703a56e0298bf5e3effeaa5d1a8e63918502
NightMeh <99148447+NightMeh@users.noreply.github.com>
2022-04-04 13:48:12 +0100

    changrs but still broke

--- ant.py
+++ ant.py
@@ -1,23 +1,68 @@
 import pygame
 import math
+SCREENHEIGHT = 720
+SCREENWIDTH = 1280
+CHUNKSIZE = 80
 
 class Ant:
     def __init__(self,x,y,rotation):
         self.x = x
         self.y = y
-        self.speed = 0.5
+        self.speed = 2
         self.rotation = rotation
+        self.antradius = 2
         self.targetx,self.targety = self.x,self.y
+        self.currentchunk = self.updatechunk()
+        visionarray = []
         
-    def createtargetlocation(self):
+        
+    def updatechunk(self):
+        return (math.floor(self.x/CHUNKSIZE),math.floor(self.y/CHUNKSIZE))
+
+    def createtargetlocation(self,movementarea):
+        count = 0
         rads = math.radians(self.rotation)
         self.targety = self.speed * math.cos(rads)
         self.targety = self.y - self.targety
         self.targetx = self.speed * math.sin(rads)
         self.targetx +=self.x
+        
+        while movementarea.collidepoint(self.targetx,self.targety) == False:
+            if count > 1:
+                self.rotation+= 30
+            rads = math.radians(self.rotation)
+            self.targety = self.speed * math.cos(rads)
+            self.targety = self.y - self.targety
+            self.targetx = self.speed * math.sin(rads)
+            self.targetx +=self.x
+            count += 1
+
+
+    def createvisionpoints(self):
+        self.visionarray = []
+        for x in range(-45,46,45):
+            rads = math.radians(self.rotation+x)
+            targety = self.speed * math.cos(rads)
+            targety = self.y - self.targety
+            targetx = self.speed * math.sin(rads)
+            targetx +=self.x
+            self.visionarray.append(Visioncone(targetx,targety,10))
+
+    def findpheramones(self,chunkdata):
+        highest = 0
+        location = 0
+        for x in range(len(self.visionarray)):
+            vision = self.visionarray[x].detectpheramone(chunkdata,self.currentchunk)
+            if vision > highest:
+                location = x
+        if location == 0:
+            self.rotation -= 2
+        elif location == 2:
+            self.rotation += 2
+
 
     def draw(self,screen):
-        pygame.draw.circle(screen, [255,0,0], (self.x,self.y), 2)
+        pygame.draw.circle(screen, [255,0,0], (self.x,self.y), self.antradius)
         pygame.draw.circle(screen, [0,0,255], (self.targetx,self.targety), 1)
 
     def findMovePerFrame(self):
@@ -29,6 +74,54 @@ class Ant:
         b = pygame.math.Vector2(self.targetx,self.targety)
         return b
 
-    def updatePosition(self,vec):
+    def updatePosition(self,vec,chunkdata):
         self.x = vec.x
         self.y = vec.y
+        self.currentchunk = self.updatechunk()
+        chunkdata[self.currentchunk].append(Pheramone((self.x,self.y),5))
+
+class Visioncone:
+    def __init__(self,x,y,width):
+        self.x = x
+        self.y = y
+        self.width = width/2
+    
+    def detectpheramone(self,chunkdata,currentchunk):
+        #loop through pheramones in chunks and adjacent
+        #find collisons iwth pheramones
+        #find strength
+        fulllist = []
+        for x in range(-1,2):
+            if currentchunk[0]-1 != -1 and currentchunk[1]+x != (SCREENHEIGHT/CHUNKSIZE)-1 and currentchunk[1]+x != -1:
+                fulllist += chunkdata[(currentchunk[0]-1,currentchunk[1]+x)]
+            if currentchunk[1]+x != -1 and currentchunk[1]+x != (SCREENHEIGHT/CHUNKSIZE)-1 and currentchunk[1]+x != -1:
+                fulllist += chunkdata[(currentchunk[0],currentchunk[1]+x)]
+            if currentchunk[0]+1 != (SCREENWIDTH/CHUNKSIZE)-1 and currentchunk[1]+x != (SCREENHEIGHT/CHUNKSIZE)-1 and currentchunk[1]+x != -1:    
+                fulllist += chunkdata[(currentchunk[0]+1,currentchunk[1]+x)]
+
+
+        for x in range(len(fulllist)):
+            if fulllist[x].rect.collidepoint(self.x,self.y) == True:
+                return fulllist[x].strength
+        return 0
+        
+
+
+class Pheramone:
+    def __init__(self,location,antradius):
+        self.x = location[0]
+        self.y = location[1]
+        self.strength = 100
+        self.strengthloss = 2
+        self.rect = pygame.Rect(self.x-antradius,self.y+antradius,2*antradius,2*antradius)
+
+    def findchunk(self,chunksize):
+        chunkx = math.floor(self.x/chunksize)
+        chunky = math.floor(self.y/chunksize)
+        return (chunkx,chunky)
+
+    def update(self):
+        self.strength-=self.strengthloss
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from turtle import update
 import ant
 import pygame
 import random
@@ -7,33 +6,43 @@ import numpy
 
 SCREENHEIGHT = 720
 SCREENWIDTH = 1280
+CHUNKSIZE = 80
+offset = 30
 
 playing = True
+chunkdata = {}
+movementarea = pygame.Rect(offset,offset,SCREENWIDTH-2*offset,SCREENHEIGHT-2*offset)
+
+for chunky in range(int(SCREENHEIGHT/CHUNKSIZE)):
+    for chunkx in range(int(SCREENWIDTH/CHUNKSIZE)):
+        chunkdata[(chunkx,chunky)] = []
+
 screen = pygame.display.set_mode([SCREENWIDTH, SCREENHEIGHT])
 screen.fill([0, 0, 0])
 pygame.display.flip()
 clock = pygame.time.Clock()
 
-antcount = 400
+antcount = 40
 ants = []
 
 for x in range(int(antcount)):
     ants.append(ant.Ant(250,250,40))
 
 antsplit = numpy.array_split(ants,4)
+varience = 30
 
 def updateants(ants,screen):
     for ant in ants:
-        ant.createtargetlocation()
+        ant.createvisionpoints()
+        ant.findpheramones(chunkdata)
+        ant.createtargetlocation(movementarea)
         stepvec = ant.findMovePerFrame()
-        ant.updatePosition(stepvec)
+        ant.updatePosition(stepvec,chunkdata)
         ant.draw(screen)
-        ant.rotation += random.randint(-30,30)
-
-
+        ant.rotation += random.randint(-varience,varience)
 
 while playing:
-    clock.tick(200)
+    clock.tick(60)
     t1 = threading.Thread(target=updateants, args=[list(antsplit[0]),screen])
     t2 = threading.Thread(target=updateants, args=[list(antsplit[1]),screen])
     t3 = threading.Thread(target=updateants, args=[list(antsplit[2]),screen])

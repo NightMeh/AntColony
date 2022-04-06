429ee69bd912975f1cd2d52260ad7640cff7f9f4
NightMeh <99148447+NightMeh@users.noreply.github.com>
2022-04-06 20:00:41 +0100

    Overhaul

--- ant.py
+++ ant.py
@@ -1,127 +1,46 @@
 import pygame
 import math
-SCREENHEIGHT = 720
-SCREENWIDTH = 1280
-CHUNKSIZE = 80
-
+import random
 class Ant:
-    def __init__(self,x,y,rotation):
-        self.x = x
-        self.y = y
-        self.speed = 2
-        self.rotation = rotation
-        self.antradius = 2
-        self.targetx,self.targety = self.x,self.y
-        self.currentchunk = self.updatechunk()
-        visionarray = []
+    def __init__(self,pos):
+        self.antimage = pygame.image.load(r'Assets\ant.png').convert_alpha()
+        self.maxSpeed = 100
+        self.steerStrength = 4
+        self.wanderStrength = 0.2
+        #self.target = target
+        self.position = pygame.math.Vector2(pos)
+        self.velocity = pygame.math.Vector2(0,0)
+        self.desireddir = pygame.math.Vector2(0,0)
+
+    def UpdatePosition(self,screen,angle):
+        #screen.blit(pygame.transform.rotate(self.antimage,-angle),self.position)
+        screen.blit(self.antimage,self.position)
+        print(angle)
+        pygame.display.flip()
+
+    def Update(self,clock,screen):
+        deltaTime = clock.tick(60)/1000
+        #self.desireddir = pygame.math.Vector2.normalize(self.target - self.position) #get unit vector for desired direction
+
+        angleoffset = math.radians(random.randint(0,360))
+        offset = pygame.math.Vector2(math.cos(angleoffset), math.sin(angleoffset))
+
+        self.desireddir = pygame.math.Vector2.normalize(self.desireddir+(offset*self.wanderStrength))
+
+        desiredVelocity = self.desireddir * self.maxSpeed #set desired velocity to max speed
+        desiredSteeringForce = (desiredVelocity - self.velocity) * self.steerStrength #set steer based on how fast it is wants to go
+        acceleration = desiredSteeringForce / 1
+        if pygame.math.Vector2.length(desiredSteeringForce) > self.steerStrength: #make sure its not over the limit
+            desiredSteeringForce = pygame.math.Vector2.scale_to_length(desiredSteeringForce,self.steerStrength)
         
+        self.velocity = self.velocity+acceleration*deltaTime #set velocity to accel*velocity*time elapsed since last frame
+        if pygame.math.Vector2.length(self.velocity+acceleration*deltaTime) > self.maxSpeed: #make sure its not over the limit
+            self.velocity = pygame.math.Vector2.scale_to_length(self.velocity,self.maxSpeed)
+
+        self.position += self.velocity*deltaTime #update its pos
+        angle = math.degrees(math.atan2(self.velocity.y,self.velocity.x)) #get its angle
+        self.UpdatePosition(screen,angle)
         
-    def updatechunk(self):
-        return (math.floor(self.x/CHUNKSIZE),math.floor(self.y/CHUNKSIZE))
-
-    def createtargetlocation(self,movementarea):
-        count = 0
-        rads = math.radians(self.rotation)
-        self.targety = self.speed * math.cos(rads)
-        self.targety = self.y - self.targety
-        self.targetx = self.speed * math.sin(rads)
-        self.targetx +=self.x
-        
-        while movementarea.collidepoint(self.targetx,self.targety) == False:
-            if count > 1:
-                self.rotation+= 30
-            rads = math.radians(self.rotation)
-            self.targety = self.speed * math.cos(rads)
-            self.targety = self.y - self.targety
-            self.targetx = self.speed * math.sin(rads)
-            self.targetx +=self.x
-            count += 1
-
-
-    def createvisionpoints(self):
-        self.visionarray = []
-        for x in range(-45,46,45):
-            rads = math.radians(self.rotation+x)
-            targety = self.speed * math.cos(rads)
-            targety = self.y - self.targety
-            targetx = self.speed * math.sin(rads)
-            targetx +=self.x
-            self.visionarray.append(Visioncone(targetx,targety,10))
-
-    def findpheramones(self,chunkdata):
-        highest = 0
-        location = 0
-        for x in range(len(self.visionarray)):
-            vision = self.visionarray[x].detectpheramone(chunkdata,self.currentchunk)
-            if vision > highest:
-                location = x
-        if location == 0:
-            self.rotation -= 2
-        elif location == 2:
-            self.rotation += 2
-
-
-    def draw(self,screen):
-        pygame.draw.circle(screen, [255,0,0], (self.x,self.y), self.antradius)
-        pygame.draw.circle(screen, [0,0,255], (self.targetx,self.targety), 1)
-
-    def findMovePerFrame(self):
-        """dx = x - self.x
-        dy = y - self.y
-        stepx, stepy = dx/60,dy/60
-        return stepx, stepy"""
-        a = pygame.math.Vector2(self.x,self.y)
-        b = pygame.math.Vector2(self.targetx,self.targety)
-        return b
-
-    def updatePosition(self,vec,chunkdata):
-        self.x = vec.x
-        self.y = vec.y
-        self.currentchunk = self.updatechunk()
-        chunkdata[self.currentchunk].append(Pheramone((self.x,self.y),5))
-
-class Visioncone:
-    def __init__(self,x,y,width):
-        self.x = x
-        self.y = y
-        self.width = width/2
-    
-    def detectpheramone(self,chunkdata,currentchunk):
-        #loop through pheramones in chunks and adjacent
-        #find collisons iwth pheramones
-        #find strength
-        fulllist = []
-        for x in range(-1,2):
-            if currentchunk[0]-1 != -1 and currentchunk[1]+x != (SCREENHEIGHT/CHUNKSIZE)-1 and currentchunk[1]+x != -1:
-                fulllist += chunkdata[(currentchunk[0]-1,currentchunk[1]+x)]
-            if currentchunk[1]+x != -1 and currentchunk[1]+x != (SCREENHEIGHT/CHUNKSIZE)-1 and currentchunk[1]+x != -1:
-                fulllist += chunkdata[(currentchunk[0],currentchunk[1]+x)]
-            if currentchunk[0]+1 != (SCREENWIDTH/CHUNKSIZE)-1 and currentchunk[1]+x != (SCREENHEIGHT/CHUNKSIZE)-1 and currentchunk[1]+x != -1:    
-                fulllist += chunkdata[(currentchunk[0]+1,currentchunk[1]+x)]
-
-
-        for x in range(len(fulllist)):
-            if fulllist[x].rect.collidepoint(self.x,self.y) == True:
-                return fulllist[x].strength
-        return 0
-        
-
-
-class Pheramone:
-    def __init__(self,location,antradius):
-        self.x = location[0]
-        self.y = location[1]
-        self.strength = 100
-        self.strengthloss = 2
-        self.rect = pygame.Rect(self.x-antradius,self.y+antradius,2*antradius,2*antradius)
-
-    def findchunk(self,chunksize):
-        chunkx = math.floor(self.x/chunksize)
-        chunky = math.floor(self.y/chunksize)
-        return (chunkx,chunky)
-
-    def update(self):
-        self.strength-=self.strengthloss
 
 
 
--- main.py
+++ main.py
@@ -1,62 +1,25 @@
 import ant
 import pygame
-import random
-import threading
-import numpy
 
 SCREENHEIGHT = 720
 SCREENWIDTH = 1280
-CHUNKSIZE = 80
-offset = 30
 
 playing = True
-chunkdata = {}
-movementarea = pygame.Rect(offset,offset,SCREENWIDTH-2*offset,SCREENHEIGHT-2*offset)
-
-for chunky in range(int(SCREENHEIGHT/CHUNKSIZE)):
-    for chunkx in range(int(SCREENWIDTH/CHUNKSIZE)):
-        chunkdata[(chunkx,chunky)] = []
 
 screen = pygame.display.set_mode([SCREENWIDTH, SCREENHEIGHT])
 screen.fill([0, 0, 0])
 pygame.display.flip()
+
 clock = pygame.time.Clock()
 
-antcount = 40
-ants = []
-
-for x in range(int(antcount)):
-    ants.append(ant.Ant(250,250,40))
-
-antsplit = numpy.array_split(ants,4)
-varience = 30
-
-def updateants(ants,screen):
-    for ant in ants:
-        ant.createvisionpoints()
-        ant.findpheramones(chunkdata)
-        ant.createtargetlocation(movementarea)
-        stepvec = ant.findMovePerFrame()
-        ant.updatePosition(stepvec,chunkdata)
-        ant.draw(screen)
-        ant.rotation += random.randint(-varience,varience)
+ant1 = ant.Ant((400,500))
 
 while playing:
-    clock.tick(60)
-    t1 = threading.Thread(target=updateants, args=[list(antsplit[0]),screen])
-    t2 = threading.Thread(target=updateants, args=[list(antsplit[1]),screen])
-    t3 = threading.Thread(target=updateants, args=[list(antsplit[2]),screen])
-    t4 = threading.Thread(target=updateants, args=[list(antsplit[3]),screen])
-    t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
-    t1.join()
-    t2.join()
-    t3.join()
-    t4.join()
-    pygame.display.update()
-    pygame.display.set_caption(str(clock.get_fps()))
+    clock.tick(100)
+    ant1.Update(clock,screen)
+    screen.fill([255, 255, 255])
+    
+    #ant1.target = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             playing = False

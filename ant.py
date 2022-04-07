b57c589c5a9954ff8a5301cd89cfe330ac965dfc
NightMeh <99148447+NightMeh@users.noreply.github.com>
2022-04-07 10:00:13 +0100

    Chhhanheld

--- ant.py
+++ ant.py
@@ -4,36 +4,64 @@ import random
 class Ant:
     def __init__(self,pos):
         self.antimage = pygame.image.load(r'Assets\ant.png').convert_alpha()
-        self.maxSpeed = 100
-        self.steerStrength = 4
+        self.maxSpeed = 150
+        self.steerStrength = 10
         self.wanderStrength = 0.2
         #self.target = target
         self.position = pygame.math.Vector2(pos)
         self.velocity = pygame.math.Vector2(0,0)
         self.desireddir = pygame.math.Vector2(0,0)
+        self.targetFoodList = []
+        self.targetFood = None
+        self.pickupRadius = 10
 
-    def HandleFood(self):
-        #if no target food
-            #get all food with perception radius
-            #select one of the random food if foodlist > 0
-            #get dir to food (foodpos-pos).normalize
-            #start targetting food if its in view angle
+    def WithinCircle(self,center_x,center_y,radius,x,y):
+        square_dist = (center_x - x) ** 2 + (center_y - y) ** 2
+        return square_dist <= radius ** 2
+
+    def HandleFood(self,foodList):
+        if len(self.targetFoodList) == 0:
+            for food in foodList:
+                if self.WithinCircle(self.position[0],self.position[1],1000,food.pos[0],food.pos[1]):
+                    self.targetFoodList.append(food)
+            
+            if len(self.targetFoodList) > 0:
+                self.targetFood = random.choice(self.targetFoodList)
+                print(f"{self.targetFood.pos - self.position}food")
+
+
+                self.desireddir = pygame.math.Vector2.normalize(self.targetFood.pos - self.position)
+
+                angle = math.degrees(math.atan2(self.velocity.y,self.velocity.x))
+                #select one of the random food if foodlist > 0
+                #get dir to food (foodpos-pos).normalize
+                #start targetting food if its in view angle
+        else:
+            self.desireddir = pygame.math.Vector2.normalize(self.targetFood.pos - self.position)
+
+            if pygame.math.Vector2.length(self.targetFood.pos - self.position) <= self.pickupRadius:
+                print(pygame.math.Vector2.length(self.targetFood.pos - self.position))
+                self.targetFood.AssignParent(self)
+            foodList.remove(self.targetFood)
+            self.targetFoodList = []
+            self.targetFood = None
+
+
+
         #else 
         #desireddir = targetfood -pos normal
         #pickup if within close radius
         #set food parent to ant
         #set target food as empty
 
-        print("done")
 
 
     def UpdatePosition(self,screen,angle):
-        #screen.blit(pygame.transform.rotate(self.antimage,-angle),self.position)
-        screen.blit(self.antimage,self.position)
+        screen.blit(pygame.transform.rotate(self.antimage,-angle),self.position)
+        #screen.blit(self.antimage,self.position)
         print(angle)
-        pygame.display.flip()
 
-    def Update(self,clock,screen):
+    def Update(self,clock,screen,foodList):
         deltaTime = clock.tick(60)/1000
         #self.desireddir = pygame.math.Vector2.normalize(self.target - self.position) #get unit vector for desired direction
 
@@ -55,7 +83,26 @@ class Ant:
         self.position += self.velocity*deltaTime #update its pos
         angle = math.degrees(math.atan2(self.velocity.y,self.velocity.x)) #get its angle
         self.UpdatePosition(screen,angle)
+
+        self.HandleFood(foodList)
         
 
+class Food:
+    def __init__(self,location,radius=3):
+        self.pos = pygame.math.Vector2(location[0],location[1])
+        self.radius = radius
+        self.parent = None
 
 
+    def Update(self,screen):
+        
+        if self.parent:
+            pygame.draw.circle(screen,[0,255,0],self.parent.position,self.radius)
+        else:
+            pygame.draw.circle(screen,[0,255,0],self.pos,self.radius)
+
+    def AssignParent(self,parent):
+        self.parent = parent
+
+
+    
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ SCREENHEIGHT = 720
 SCREENWIDTH = 1280
 
 playing = True
-
+foodList = []
 screen = pygame.display.set_mode([SCREENWIDTH, SCREENHEIGHT])
 screen.fill([0, 0, 0])
 pygame.display.flip()
@@ -16,10 +16,22 @@ ant1 = ant.Ant((400,500))
 
 while playing:
     clock.tick(100)
-    ant1.Update(clock,screen)
-    screen.fill([255, 255, 255])
+    
+    ant1.Update(clock,screen,foodList)
+    
+    
     
     #ant1.target = pygame.mouse.get_pos()
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             playing = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            foodList.append(ant.Food(pygame.mouse.get_pos(),3))
+
+    for food in foodList:
+        food.Update(screen)
+    
+    pygame.display.flip()
+    screen.fill([255, 255, 255])
+    
